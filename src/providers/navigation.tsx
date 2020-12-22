04162b9d946ad41837c8887451faaea4eb4f61a6
Cedric van Putten <me@bycedric.com>
2020-12-22 17:55:51 +0100

    refactor: use spaces for indentation

--- src/providers/navigation.tsx
+++ src/providers/navigation.tsx
@@ -8,10 +8,10 @@ import { OnboardingScreen } from '../screens/onboarding';
 const Stack = createStackNavigator();
 
 export const NavigationProvider: React.FC = () => (
-	<NavigationContainer>
-		<Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="Onboarding" component={OnboardingScreen} />
-			<Stack.Screen name="Distance" component={DistanceScreen} />
-		</Stack.Navigator>
-	</NavigationContainer>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      <Stack.Screen name="Distance" component={DistanceScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
