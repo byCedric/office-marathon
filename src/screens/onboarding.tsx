e5d5cfed628476fd509a1c419a777495ef0a10d7
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-02-19 12:35:59 -0800

    Changed permission logic

--- src/screens/onboarding.tsx
+++ src/screens/onboarding.tsx
@@ -8,7 +8,7 @@ export const OnboardingScreen: React.FC = () => {
   const navigation = useNavigation();
   const [permission, askPermission] = usePermissions(LOCATION);
 
-  const permScope = permission?.permissions.location.scope; 
+  const permScopeAlways = permission?.permissions.location.scope == "always"; 
 
   const onContinue = useCallback(() => {
     navigation.navigate('Distance');
@@ -17,12 +17,12 @@ export const OnboardingScreen: React.FC = () => {
   useEffect(() => {
     // Only redirect on first render or permission change,
     // not when users go back to this screen.
-    if (permScope == "always") {
+    if (permission?.granted && permScopeAlways) {
       onContinue();
     }
-  }, [permScope]);
+  }, [permission?.granted, permScopeAlways]);
 
-  if (permScope == "always") {
+  if (permission?.granted && permScopeAlways) {
     return (
       <Box variant='page'>
         <Box>
@@ -38,12 +38,16 @@ export const OnboardingScreen: React.FC = () => {
     <Box variant='page'>
       <Box>
         <Title>We need your permission</Title>
-        <Paragraph>To monitor your office marathon, we need access to background location.</Paragraph>
+        <Paragraph>To monitor your office marathon, we need access to background location. Please select ALWAYS ALLOW location services in your settings.</Paragraph>
       </Box>
       {!permission
         ? <Spinner />
         : <Button onPress={askPermission}>Grant permission</Button>
       }
+      
+      {permission?.granted && !permScopeAlways &&
+          <Paragraph>You have only granted permission to use your location when the app is in use. Please ALWAYS ALLOW location services in your settings.</Paragraph>
+        }
     </Box>
   );
 };
