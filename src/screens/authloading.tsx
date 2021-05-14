fe3aa939349fd7360bc7f2f5042ff49a46f1e7e5
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-05-14 12:52:12 -0700

    authentication screen redirect to onboarding

--- src/screens/authloading.tsx
+++ src/screens/authloading.tsx
@@ -1,12 +1,18 @@
 // import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { API, Auth, graphqlOperation } from 'aws-amplify';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { createUser } from '../graphql/mutations';
 import { getUser } from '../graphql/queries';
 import { Box, Paragraph, Spinner, Title } from '../providers/theme';
 
 export const AuthLoadingScreen: React.FC = () => {
-    // const navigation = useNavigation();
+    const navigation = useNavigation();
+
+    const onContinue = useCallback(() => {
+        navigation.navigate('Onboarding');
+    }, [navigation]);
+
     //run this snippet only when App is first mounted
     useEffect( () => { // around the 1 hour mark in tutorial
         const fetchUser = async () => {
@@ -22,6 +28,7 @@ export const AuthLoadingScreen: React.FC = () => {
                     console.log("User is already registered in database")
                 } 
 
+            // if there is no user in our database with that ID, then create one
                 const newUser = {
                     id: userInfo.attributes.sub,
                     email: userInfo.attributes.email,
@@ -33,11 +40,13 @@ export const AuthLoadingScreen: React.FC = () => {
                         { input: newUser }
                     )
                 )
-            // if there is no user in our database with that ID, then create one
+
             }
         }
         fetchUser();
+        onContinue();
     }, [])
+
 
     //ended tutorial(https://www.youtube.com/watch?v=HdLbavvfflQ&list=PLKrxTdwfEw2WjZ4rfVM_K-RdEJkvWi36u&index=15&t=2531s) 
     // around 1hr 20 min. Recommended start time: 1hr 34 min
@@ -46,7 +55,7 @@ export const AuthLoadingScreen: React.FC = () => {
         <Box variant='page'>
             <Box>
                 <Title>Authenticating</Title>
-                <Paragraph>Please wait while we authenticate your stuff</Paragraph>
+                <Paragraph>Please wait while we authenticate your information</Paragraph>
             </Box>
             <Spinner />
         </Box>
