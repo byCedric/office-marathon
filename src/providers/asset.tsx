ddc0b8492c0cfa9044a07f2380bd12d343be716f
Cedric van Putten <github@cedric.dev>
2023-03-17 13:42:42 +0100

    refactor: replace app-loading with splash-screen

--- src/providers/asset.tsx
+++ src/providers/asset.tsx
@@ -1,14 +1,20 @@
 import { useFonts, OpenSans_600SemiBold, OpenSans_400Regular } from '@expo-google-fonts/open-sans';
-import AppLoading from 'expo-app-loading';
-import React from 'react';
+import SplashScreen from 'expo-splash-screen';
+import { useEffect, PropsWithChildren } from 'react';
 
-export const AssetProvider: React.FC = (props) => {
+// Prevent the splash screen from hiding automatically
+SplashScreen.preventAutoHideAsync();
+
+export function AssetProvider(props: PropsWithChildren) {
   const [isLoaded] = useFonts({
     'open-sans-regular': OpenSans_400Regular,
     'open-sans-semibold': OpenSans_600SemiBold,
   });
 
-  return isLoaded
-    ? <>{props.children}</>
-    : <AppLoading />;
+  useEffect(() => {
+    // Hide the splash screen when the fonts are loaded
+    if (isLoaded) SplashScreen.hideAsync();
+  }, [isLoaded]);
+
+  return !isLoaded ? null : <>{props.children}</>;
 };
